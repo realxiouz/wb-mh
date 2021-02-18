5ab67b1437987e317b1ddbf77a3be509ac5da6cd
qin <xiouz@163.com>
2021-02-18 17:05:02 +0800

    .

--- pages/message/index.vue
+++ pages/message/index.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>message</div>
+  <div>
+    <div class="header">
+      <div :style="{ height: `${navBarHeight}px`, padding: navBarPadding }" >
+
+			</div>
+      <div style="height:300rpx;" class=""></div>
+    </div>
+    <shopro-login-modal />
+    <shopro-tabbar />
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
-
+  computed: {
+    ...mapState('device', ['navBarHeight', 'navBarPadding']),
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.header{
+  background: linear-gradient(180deg, rgba(239, 196, 128, 1) 0%, rgba(248, 220, 165, 1) 25%, rgba(255, 255, 255, 1) 98%);
+}
 </style>
